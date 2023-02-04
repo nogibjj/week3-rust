a497d4dc0f53bf76046e78e53f84dc6ecf6b1cdc
Airu Song <63376516+airusong@users.noreply.github.com>
2023-02-04 21:03:36 +0000

    add

---
+++ src/main.rs
@@ -0,0 +1,20 @@
+fn main(){
+    const N: i32=100;
+    let mut count=0;
+    for i in 1..N/2{
+        let mut temp=true;
+        for j in 2..i/2+1{
+            if i%j==0 {
+                temp=false;
+                break;
+            }    
+        }
+        if temp{
+            count+=1;
+            print!("{} ",i);
+            if count%5==0 {
+                println!();
+            }
+        }
+    }
+}
